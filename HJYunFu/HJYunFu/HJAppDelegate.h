99b910623abc9417ab4439a4ad471fccf0a02a6e
zhjch <zhjch80@163.com>
2014-07-13 13:48:46 +0800

    no message

---
+++ HJYunFu/HJYunFu/HJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HJAppDelegate.h
+//  HJYunFu
+//
+//  Created by 华晋传媒 on 14-7-13.
+//  Copyright (c) 2014年 HuaJinMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HJYunFu/HJYunFu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HJYunFu
+//
+//  Created by 华晋传媒 on 14-7-13.
+//  Copyright (c) 2014年 HuaJinMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HJAppDelegate class]));
+    }
+}
